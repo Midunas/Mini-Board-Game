35b775d21532a17572e80d576031d6527ee274a2
Midunas <mykolas.vidunas@codeacademylt.onmicrosoft.com>
2022-10-05 17:37:35 +0300

    feat(109): segregated character display from choose character, removed SideBar component

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,13 +1,16 @@
 import GameBoard from "../components/GameBoard/GameBoard";
 import RollDice from "../components/Dice/RollDice";
+import { Box } from "@mui/material";
 
 const GamePage = () => {
 
   return (
-    <div className="d-flex">
-      <GameBoard />
-      <RollDice />
-    </div>
+    <Box sx={{ m: 'auto' }}>
+      <div className="d-flex">
+        <GameBoard />
+        <RollDice />
+      </div>
+    </Box>
   );
 };
 
